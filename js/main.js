faea4bad80754c299f1b6ce68f95fbbb24bd8e53
dj-diaz <denniel.joshua@gmail.com>
2016-03-14 20:24:04 +0800

    fix clock

--- js/main.js
+++ js/main.js
@@ -317,7 +317,7 @@ var clock;
 	var currentDate = new Date();
 
 	// Set some date in the future. In this case, it's always Jan 1
-	futureDate = new Date("May 22, 2016 00:00:00")
+	futureDate = new Date("March 22, 2016 00:00:00")
 
 	// Calculate the difference in seconds between the future and current date
 	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
